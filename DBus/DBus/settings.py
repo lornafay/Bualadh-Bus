294bc38525ad7f36dd16ac85b9f769fc114295b7
Lorna <student@ipa-009.137.43.116.17>
2022-06-19 12:59:31 +0000

    docker image builds successfully, need to configure with db to run

--- DBus/DBus/settings.py
+++ DBus/DBus/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from DBus.credentials import Password_DB_SSH as pw
+from DBus.credentials import SECRET_KEY as sk
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j*l6#-76=!ny&-(obs3=6)$d*z+^(4_gp3_-k5gvwv=5)cgn0h'
+SECRET_KEY = sk
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
