f9ae8faf92f83be2982bf2114a7eddab3459cf47
ChihLun <59338015+Chih-lun@users.noreply.github.com>
2022-06-27 16:42:31 +0100

    Setting up React framework + Bootstrap with Django
    This includes the integration with react and django.

---
+++ django/core/credentials.py
@@ -0,0 +1,3 @@
+# once credentials are complete and on UCD remote add to .gitignore
+Password_DB_SSH= '!bBt8-2022**'
+SECRET_KEY = "django-insecure-j*l6#-76=!ny&-(obs3=6)$d*z+^(4_gp3_-k5gvwv=5)cgn0h"
