cbf668aa5862560b08d95f472ad44b352e1c1156
Lorna Fay <74617821+lornafay@users.noreply.github.com>
2022-07-07 13:07:50 +0100

    Delete django directory

--- django/core/credentials.py
+++
@@ -1,3 +0,0 @@
-# once credentials are complete and on UCD remote add to .gitignore
-Password_DB_SSH= '!bBt8-2022**'
-SECRET_KEY = "django-insecure-j*l6#-76=!ny&-(obs3=6)$d*z+^(4_gp3_-k5gvwv=5)cgn0h"
