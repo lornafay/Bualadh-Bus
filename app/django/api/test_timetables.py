01ce5eb3725410d169afd9673a5d6e61ab325c3f
Porterfa <Porterfa@tcd.ie>
2022-07-20 14:04:02 +0100

    more bad dummy code and trying out testing

---
+++ app/django/api/test_timetables.py
@@ -0,0 +1,33 @@
+from cgitb import reset
+from time import time
+import unittest
+from unittest import result
+import timetables as tb
+import pandas as pd
+import numpy as np
+
+class TestQuery(unittest.TestCase):
+
+    def test_extract_route_ID(self):
+        timetable = tb.Display_timetables()
+        method = timetable.extract_route_ID()
+
+        self.assertTrue(list(method.keys()) == ['ROUTEID', 'LINEID'])
+        self.assertTrue(type(method)) == pd.core.frame.DataFrame
+    
+    def test_get_timetable(self):
+        timetable = tb.Display_timetables()
+        method = timetable.get_timetable()
+
+        self.assertTrue(list(method.keys()) == ['ROUTEID', 'LINEID', 'DAY_OF_WEEK', 'PLANNED_DEP_R_M5'])
+        self.assertTrue(type(method)) == pd.core.frame.DataFrame
+
+    def test_return_timetable(self):
+        timetable = tb.Display_timetables()
+        method = timetable.return_timetable()
+
+        self.assertTrue(list(method.keys()) == ['LINEID', 'PLANNED_DEP_R_M5'])
+        self.assertTrue(type(method)) == pd.core.frame.DataFrame
+
+if __name__== '__main__':
+    unittest.main()
--- app/django/api/timetables.py
+++ app/django/api/timetables.py
@@ -1,46 +1,74 @@
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 import json
 import os
 from query import Query
+import pandas as pd
 #import ParseArguments
 #import PredictJourneyTimes
 
+StopID = '365'
+Day = 'Saturday'
+
 class Display_timetables:
+    """ Class to display timetables to user.
+    Contains three methods for extracting routeID, getting timetable, and returning timetable"""
 
-    def extract_route_ID(StopPointID):
-
+    def extract_route_ID(StopID):
+        """Method to extract_route_ID and append to LineID
+        Returns dataframe for line and route IDs"""
+        
         #Get line id from django after user input
         # ParseArguements.LineID()
 
-        #get line and route id together?
-        # PredictJourneyTimes.Get_LineID_RouteID_Groups(RouteID)
+        #get line and route id together and possibly drop result
+        # PredictJourneyTimes.Get_LineID_RouteID_Groups()
 
-        #return RouteIDs_list
-        return ["77A_30", "47_136", "1_40"]
+        dictionary = {
+            'ROUTEID' : ['77A_30', '77A_29', '46_5'],
+            'LINEID' : ['77A', '77A', '46']
+        }
+
+        df = pd.DataFrame(dictionary)
         
-    def get_timetables():
+        #return route ID list
+        return df
+    
+    def get_timetable(Day):
+        """Method to get day of week from user input
+        Will query database for planned departure times for line
+        Returns dataframe containing routeID, day of week, planned departure times"""
         
-        #get day of week
-        # ParseArguements.Extract_Day_of_Week()
-
-        route_ID = extract_route_ID()
-
-        for i in len(route_ID):
-            
-            #query database for routeID and Day of week:
-                #SELECT * FROM static_tables.timetables Where ROUTEID = "41B_52"
-                # and DAY_OF_WEEK = "Tuesday" order by PLANNEDTIME_DEP_R_M5 
-
-            return database
-
-    def return_time_table():
+        #takes day of week from user
+        day_of_week = 'Saturday'
         
-        #get database that has all times for routeid on day of week
-        timetable = get_timetables()
+        #after querying DB with SELECT ROUTEID, DAY_OF_WEEK, PLANNEDTIME_DEP_R_M5 FROM static_tables.timetables Where ROUTEID = '77A_30'and DAY_OF_WEEK = "Tuesday" order by PLANNEDTIME_DEP_R_M5
+        
+        dictionary = {
+            'ROUTEID' : ['77A_30', '77A_30', '77A_29', '46_5'],
+            'LINEID' : ['77A', '77A', '77A', '46'],
+            'DAY_OF_WEEK' : ['SATURDAY', 'SATURDAY', 'SATURDAY', 'SATURDAY'],
+            'PLANNED_DEP_R_M5' : ['08:00', '08:10', '08:10', '08:20']
+        }
+        
+        df = pd.DataFrame(dictionary)
+        
+        return df
+    
+    def return_timetable(self):
+        """Method to finalize dataframe to display to user
+        returns dataframe containing only times of departure for that line"""
+    
+        df = Display_timetables.get_timetable(self)
+        
+        #drop columns don't want to display and any duplicates
+        df = df.drop('DAY_OF_WEEK', axis=1)
+        df = df.drop('ROUTEID', axis=1)
+        df = df.drop_duplicates(subset=['PLANNED_DEP_R_M5'])
+        
+        return df
 
-        #somehow concat that to a table but not clear on how to do until other methods done
-
-        #pandas to drop routeID column and any duplicate rows
-
-        return timetable
+time = Display_timetables()
+print(time.extract_route_ID())
+print(time.get_timetable())
+print(time.return_timetable())
 
