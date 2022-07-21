881f193d112927d0b027f18ffddeef600d8b12be
Porterfa <Porterfa@tcd.ie>
2022-07-21 18:58:48 +0100

    bad code don't look

--- app/django/api/timetables.py
+++ app/django/api/timetables.py
@@ -1,13 +1,13 @@
-# from sqlalchemy import create_engine
 import json
 import os
-# from query import Query
+from query import Query
 import pandas as pd
-#import ParseArguments
-#import PredictJourneyTimes
+from parse_arguments import Parse_arguments as pa
+from journey_times import JourneyTimes as jt
 
-StopID = '365'
-Day = 'Saturday'
+#
+stopID = '365'
+day = 'Saturday'
 
 class DisplayTimetables:
     """Class to display timetables to user.
@@ -15,40 +15,43 @@ class DisplayTimetables:
     Contains three methods for extracting routeID, getting timetable, and returning timetable.
     """
 
-    def extract_route_ID(StopID):
+    def extract_route_ID(stopID):
         """Method to extract_route_ID and append to LineID.
         
         Returns dataframe for line and route IDs.
         """
         
         # Get line id from django after user input
-        # ParseArguements.LineID()
+        lineID = pa.get_lineid(stopID)
 
-        # Get line and route id together and possibly drop result
-        # PredictJourneyTimes.Get_LineID_RouteID_Groups()
-
-        dictionary = {
-            'ROUTEID' : ['77A_30', '77A_29', '46_5'],
-            'LINEID' : ['77A', '77A', '46']
-        }
-
-        df = pd.DataFrame(dictionary)
+        # Get line and route id together and drop result
+        df = jt.parse_routeID_lineID(lineID)
+        df = df.drop('result', axis=1)
         
-        # Return route ID list
+        # Return routeID to LineID list
         return df
     
-    def get_timetable(Day):
+    def get_timetable(day):
         """Method to get day of week from user input.
         
         Will query database for planned departure times for line.
         Returns dataframe containing routeID, day of week, planned departure times.
         """
         
-        # Takes day of week from user
-        day_of_week = 'Saturday'
+        # Takes day of week from user and return day of week method from ParseArguments class
+        day_of_week = pa.extract_day_of_week(day)
+
+        #extract routeid method from above
+        routeID = DisplayTimetables.extract_route_ID(stopID)
+        routeID = routeID["ROUTEID"].unique()
         
         # After querying DB with SELECT ROUTEID, DAY_OF_WEEK, PLANNEDTIME_DEP_R_M5 FROM static_tables.timetables Where ROUTEID = '77A_30'and DAY_OF_WEEK = "Tuesday" order by PLANNEDTIME_DEP_R_M5
+        query= Query()
+        DBs ="static_tables"
+        retreive_DB = query.get_engine(DBs)
         
+        df = pd.read_sql("SELECT ROUTEID, DAY_OF_WEEK, PLANNEDTIME_DEP_R_M5 FROM static_tables.timetables Where ROUTEID = %s and DAY_OF_WEEK = %s order by PLANNEDTIME_DEP_R_M5' )  ", retreive_DB);
+
         dictionary = {
             'ROUTEID' : ['77A_30', '77A_30', '77A_29', '46_5'],
             'LINEID' : ['77A', '77A', '77A', '46'],
