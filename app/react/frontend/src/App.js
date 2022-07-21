942b0e25b3e7fd88d8bf4a842e055df4fa384018
Porterfa <Porterfa@tcd.ie>
2022-07-21 10:53:25 +0100

    re-styling quick fix to get it done

--- app/react/frontend/src/App.js
+++ app/react/frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       {/* web sections */}
       <Navigation />
-      <Cover />
+      {/* <Cover /> */}
       <Home />
       <Faq />
       <Timetables />
