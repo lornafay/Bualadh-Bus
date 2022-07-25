38e2a8272ba329b4952a425bf9483d602f7a6fdc
ChihLun <59338015+Chih-lun@users.noreply.github.com>
2022-07-25 12:08:25 +0100

    test parse_arguments

--- app/django/api/test_parse_arguments.py
+++ app/django/api/test_parse_arguments.py
@@ -12,6 +12,7 @@ class TestQuery(unittest.TestCase):
         result = parse_arguments.parse_date()
 
         self.assertTrue(type(result) == pd._libs.tslibs.timestamps.Timestamp)
+        self.assertEquals(result, pd.to_datetime('2022-07-23T12:00:00.000Z'))
 
 
     def test_extract_day_of_week(self):
@@ -20,6 +21,7 @@ class TestQuery(unittest.TestCase):
         result = parse_arguments.extract_day_of_week()
 
         self.assertTrue(type(result) == str)
+        self.assertEquals(result, 'Saturday')
 
         
     def test_transform_to_forecast_date(self):
@@ -28,6 +30,7 @@ class TestQuery(unittest.TestCase):
         result = parse_arguments.transform_to_forecast_date()
         
         self.assertTrue(type(result) == pd._libs.tslibs.timestamps.Timestamp)
+        self.assertEquals(result, pd.to_datetime('2022-07-23 12:00:00'))
 
 
     def test_get_beginningstop(self):
@@ -36,6 +39,7 @@ class TestQuery(unittest.TestCase):
         result = parse_arguments.get_beginningstop()
         
         self.assertTrue(type(result) == str)
+        self.assertEquals(result, '395')
 
 
     def test_get_endingstop(self):
@@ -44,6 +48,7 @@ class TestQuery(unittest.TestCase):
         result = parse_arguments.get_endingstop()
         
         self.assertTrue(type(result) == str)
+        self.assertEquals(result, '4662')
 
 
     def test_get_lineid(self):
@@ -52,6 +57,7 @@ class TestQuery(unittest.TestCase):
         result = parse_arguments.get_lineid('77A')
 
         self.assertTrue(type(result) == str)
+        self.assertEquals(result, '77A')
 
 if __name__== '__main__':
     unittest.main()
