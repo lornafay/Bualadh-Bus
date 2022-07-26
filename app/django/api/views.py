39b3193b2e7a09f46070d2e8bcb5f47c8b25e37d
Porterfa <Porterfa@tcd.ie>
2022-07-26 13:19:24 +0100

    na

--- app/django/api/views.py
+++ app/django/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from django.shortcuts import render
 from bus.models import current_weather
 from .serializers import Weather_Serializer, TimetableSerializer
-from timetables import DisplayTimetables
+from api.timetables import DisplayTimetables
 import pandas as pd
 import json
 
@@ -19,32 +19,21 @@ def get_current_weather(request):
 @api_view(['POST'])
 def user_input(request):
     print(request.data)
-    return Response({'status':'successful'})
+    time = request.data['time']
+    start = request.data['location']
+    end = request.data['destination']
+    lst = [time, start, end]
+    print('request: ', lst[0], lst[1], lst[2])
+    j = JourneyTimes(lst)
+    p=j.predict_total_journey_time()
 
-class Timetables(APIView):
-    """ Method Get and Post timetables to frontend
+    return Response({'result':p})
 
-    Two methods: the Get Request to first display on page,
-    and the Post request that takes user input and updates page.
-    """
+@api_view(['GET'])
+def get_timetable(request):
+    df = DisplayTimetables.return_timetable('395', '395', 'Saturday')
+    return Response(df)
 
-    serializer_class = TimetableSerializer
-
-    def get(self, request, format=None):
-        """ Returns timetable that first displays on frontend.
-        """
-        df = DisplayTimetables.return_timetable('395', '395', 'Saturday')
-        return Response(df)
-
-    def post(self, request):
-        """ Method to take user input and return timetable for that bus stop. """
-
-        serializer = TimetableSerializer(data = request.data)
-        
-        if serializer.is_valid():
-            day = serializer.data.get('day')
-            stopID = serializer.data.get('stopID')
-            df_new = DisplayTimetables.return_timetable('395', stopID, day)
-            return Response(df_new)
-        else:
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+@api_view(['POST'])
+def user_timetable(request):
+    pass
