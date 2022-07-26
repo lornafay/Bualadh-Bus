db501690e85c9c7012b51cfee6ca26266da69c5a
Lorna Fay <lorna.fay@ucdconnect.ie>
2022-07-26 23:20:05 +0100

    handling conflicts and updating import paths where needed

--- app/django/api/serializers.py
+++ app/django/api/serializers.py
@@ -1,23 +1,12 @@
 from rest_framework import serializers
 from bus.models import current_weather
-<<<<<<< HEAD
-# from api.timetables import DisplayTimetables
-=======
-from timetables import DisplayTimetables
->>>>>>> develop
+from api.timetables import DisplayTimetables
 
 class Weather_Serializer(serializers.ModelSerializer):
     class Meta:
         model = current_weather
         fields = '__all__'
 
-<<<<<<< HEAD
-# class TimetableSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = DisplayTimetables.return_timetable('395', '395', 'Saturday')
-#         fields = '__all__'
-=======
 class TimetableSerializer(serializers.Serializer):
     day = serializers.CharField(max_length=10)
     stopID = serializers.CharField(max_length=10)
->>>>>>> develop
