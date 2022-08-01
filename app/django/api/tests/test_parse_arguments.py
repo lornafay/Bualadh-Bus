c47e163addbc876817d37fb27845abc77edb5eb2
Lorna Fay <lorna.fay@ucdconnect.ie>
2022-08-01 12:47:47 +0100

    corrected forecast date tester to be generalisable

--- app/django/api/tests/test_parse_arguments.py
+++ app/django/api/tests/test_parse_arguments.py
@@ -32,12 +32,25 @@ class TestQuery(unittest.TestCase):
     def test_transform_to_forecast_date(self):
 
         tomorrow = datetime.now() + timedelta(days=1)
+        hour = tomorrow.hour
+        # find out which forecast time it is closest to
+        forecast_hours = [0, 6, 12, 18]
+        diff = 6
+        set_hour = 0
+        for fh in forecast_hours:
+            if abs(hour - fh) < diff:
+                diff = abs(hour - fh)
+                set_hour = fh
+        tomorrow = tomorrow.replace(hour=set_hour, minute=00, second=00, microsecond=00)
+        
         lst = [str(tomorrow), '395', '4662']
         parse_arguments = Parse_arguments(lst)
         result = parse_arguments.transform_to_forecast_date()
-        
+        print("result: ", result)
+        print("tomorrow: ", tomorrow)
         self.assertTrue(type(result) == pd._libs.tslibs.timestamps.Timestamp)
         self.assertEquals(result, tomorrow)
+        
 
 
     def test_get_beginningstop(self):
