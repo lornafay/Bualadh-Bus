c05a81ef1486ce31efd3561e072c6138121bb323
Lorna Fay <lorna.fay@ucdconnect.ie>
2022-08-07 15:29:48 +0100

    using users journey segment only

--- app/django/api/route_map.py
+++ app/django/api/route_map.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from bus.models import stop_locations as sl
 from .query import Query
 import pandas as pd
@@ -21,13 +22,13 @@ class RouteMap():
         df = pd.read_sql("SELECT DISTINCT STOPPOINTID FROM static_tables.{0}_timetable_V2 WHERE ROUTEID IN (SELECT DISTINCT ROUTEID FROM static_tables.{0}_timetable_V2 WHERE ROUTEID IN (SELECT ROUTEID FROM static_tables.{0}_timetable_V2 WHERE LINEID = '{1}' AND STOPPOINTID = {2}) AND ROUTEID IN (SELECT ROUTEID FROM static_tables.{0}_timetable_V2 WHERE LINEID = '{1}' AND STOPPOINTID = {3})) ORDER BY TRIPS_TIME_PROPORTION_v2;".format(self.day, self.line, self.start, self.end), static_tables)
 
         stop_sequence = list(df['STOPPOINTID'].astype(int))
-
         # code to narrow route down to user's segment only, does not work very well in practice
-        #start_ind = stop_sequence.index(self.start)
-        #end_ind = stop_sequence.index(self.end)
-        #user_journey_stops = stop_sequence[start_ind:end_ind+1]
+        start_ind = stop_sequence.index(self.start)
+        end_ind = stop_sequence.index(self.end)
+        user_journey_stops = stop_sequence[start_ind:end_ind+1]
         print("stop sequence: ", stop_sequence)
-        return stop_sequence
+            
+        return user_journey_stops
 
         
     def get_intermediate_stop_locations(self):
