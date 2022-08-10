97f9954f8d2717bd508877c1801bef396123dd75
Porterfa <Porterfa@tcd.ie>
2022-08-10 11:07:26 +0100

    styling and component changes

--- app/react/frontend/src/App.js
+++ app/react/frontend/src/App.js
@@ -2,7 +2,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import components
 import Navigation from './Components/Navigation/Navigation';
-import Cover from './Components/Cover/Cover'
 import Home from './Components/Home/Home';
 import Faq from './Components/Faq/Faq';
 import Timetables from './Components/Timetables/Timetables';
@@ -14,10 +13,9 @@ function App() {
     <div className="App">
       {/* web sections */}
       <Navigation />
-      {/* <Cover /> */}
       <Home />
-      <Faq />
       <Timetables />
+      <Faq />
       <Aboutus />
       <Footer />
     </div>
